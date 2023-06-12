9908e0c53f3d316a5c781c8d00c0ab9eb5c7cb58
Mastermind <dykhnodanylo10@gmail.com>
2023-06-12 15:08:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+const input = document.querySelector("#phone-input");
+let iti = window.intlTelInput(input, {
+  initialCountry: "auto",
+  geoIpLookup: callback => {
+    fetch("https://ipapi.co/json")
+      .then(res => res.json())
+      .then(data => callback(data.country_code))
+      .catch(() => callback("us"));
+    },
+});
